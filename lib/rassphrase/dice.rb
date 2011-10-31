f5754150c68253ade9a790e4b296e47bb7775689
Arturo Guzman <arturo@kisurfer.com>
2011-10-31 13:31:45 -0600

    Add Dice#roll_count

--- lib/rassphrase/dice.rb
+++ lib/rassphrase/dice.rb
@@ -1,12 +1,17 @@
 module Rassphrase
   class Dice
-    
     def initialize
       @random = Random.new
+      @roll_count = 0
     end
 
     def roll
+      @roll_count += 1
       @random.rand(1..6)
+    end
+
+    def roll_count
+      @roll_count
     end
   end
 end
--- spec/rassphrase/dice_spec.rb
+++ spec/rassphrase/dice_spec.rb
@@ -2,12 +2,25 @@ require 'spec_helper'
 
 module Rassphrase
   describe Dice do
+
+    let(:dice) { Dice.new }
+
     describe "#roll" do
       it "returns a number between 1 and 6" do
-        dice = Dice.new
         roll = dice.roll
         roll.should be >= 1
         roll.should be <= 6
+      end
+    end
+
+    describe "#roll_count" do
+      it "returns the number of times the dice has rolled" do
+        dice.roll
+        dice.roll_count.should == 1
+        2.times { dice.roll }
+        dice.roll_count.should == 3
+        5.times { dice.roll }
+        dice.roll_count.should == 8
       end
     end
   end
